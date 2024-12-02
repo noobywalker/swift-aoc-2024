95acb51c69a98cf3d8812c4664a6b1def0b7795c
Waratnan Suriyasorn <waratnan.suriyasorn@auto1.com>
2024-12-02 17:49:29 +0100

    Advent of code 2024 - Day02

--- Sources/Day02.swift
+++ Sources/Day02.swift
@@ -1,35 +1,27 @@
 import Algorithms
 
 struct Day02: AdventDay {
-
+  
   var data: String
-
-  var entities: (lList: [Int], rList: [Int]) {
-    var lList: [Int?] = []
-    var rList: [Int?] = []
-    data.split(separator: "\n")
-      .forEach {
-        let pair = $0.split(separator: "   ").compactMap { Int($0) }
-        lList.append(pair.first)
-        rList.append(pair.last)
+  
+  var entities: [[Int]] {
+    return data.split(separator: "\n")
+      .map {
+        $0.split(separator: " ").compactMap { Int($0) }
       }
-    return (
-      lList: lList.compactMap { $0 },
-      rList: rList.compactMap { $0 }
-    )
   }
-
+  
   /**
    --- Day 2: Red-Nosed Reports ---
-
+   
    Fortunately, the first location The Historians want to search isn't a long walk from the Chief Historian's office.
-
+   
    While the Red-Nosed Reindeer nuclear fusion/fission plant appears to contain no sign of the Chief Historian, the engineers there run up to you as soon as they see you. Apparently, they still talk about the time Rudolph was saved through molecular synthesis from a single electron.
-
+   
    They're quick to add that - since you're already here - they'd really appreciate your help analyzing some unusual data from the Red-Nosed reactor. You turn to check if The Historians are waiting for you, but they seem to have already divided into groups that are currently searching every corner of the facility. You offer to help with the unusual data.
-
+   
    The unusual data (your puzzle input) consists of many reports, one report per line. Each report is a list of numbers called levels that are separated by spaces. For example:
-
+   
    7 6 4 2 1
    1 2 7 8 9
    9 7 6 2 1
@@ -37,13 +29,13 @@ struct Day02: AdventDay {
    8 6 4 4 1
    1 3 6 7 9
    This example data contains six reports each containing five levels.
-
+   
    The engineers are trying to figure out which reports are safe. The Red-Nosed reactor safety systems can only tolerate levels that are either gradually increasing or gradually decreasing. So, a report only counts as safe if both of the following are true:
-
+   
    The levels are either all increasing or all decreasing.
    Any two adjacent levels differ by at least one and at most three.
    In the example above, the reports can be found safe or unsafe by checking those rules:
-
+   
    7 6 4 2 1: Safe because the levels are all decreasing by 1 or 2.
    1 2 7 8 9: Unsafe because 2 7 is an increase of 5.
    9 7 6 2 1: Unsafe because 6 2 is a decrease of 4.
@@ -51,21 +43,96 @@ struct Day02: AdventDay {
    8 6 4 4 1: Unsafe because 4 4 is neither an increase or a decrease.
    1 3 6 7 9: Safe because the levels are all increasing by 1, 2, or 3.
    So, in this example, 2 reports are safe.
-
+   
    Analyze the unusual data from the engineers. How many reports are safe?
    */
-
-  func part1() -> Any {
-    return 0
+  
+  enum TolerateLevelType {
+    case increasing
+    case decreasing
   }
-
+  
+  private func isSafeFor(type: TolerateLevelType, current: Int, next: Int) -> Bool {
+    let diff = next - current
+    guard diff != 0, abs(diff) <= 3 else {
+      // neither an increase or a decrease or greater than three
+      return false
+    }
+    
+    switch type {
+    case .increasing where diff < 0:
+      return false
+    case .decreasing where diff > 0:
+      return false
+    default:
+      return true
+    }
+  }
+  
+  private func safeCheck(for levels: [Int], autoFix: Bool = false) -> Bool {
+    guard !levels.isEmpty else { return false }
+    var type: TolerateLevelType = .increasing
+    for idx in 0..<levels.count-1 {
+      let current = levels[idx]
+      let next = levels[idx+1]
+      if idx == 0 {
+        type = next > current ? .increasing : .decreasing
+      }
+      
+      guard isSafeFor(type: type, current: current, next: next) else {
+        guard autoFix else { return false }
+        var dropFirst: [Int] = []
+        var dropCurrent = levels
+        var dropNext = levels
+        
+        if idx > 0 {
+          dropFirst = levels
+          dropFirst.remove(at: 0)
+        }
+        
+        dropCurrent.remove(at: idx)
+        dropNext.remove(at: idx+1)
+        
+        return safeCheck(for: dropFirst)
+        || safeCheck(for: dropCurrent)
+        || safeCheck(for: dropNext)
+      }
+    }
+    return true
+  }
+  
+  func part1() -> Any {
+    return entities
+      .filter { safeCheck(for: $0) }
+      .count
+  }
+  
   /**
-   -- Part Two ---
-
-
+   --- Part Two ---
+   
+   The engineers are surprised by the low number of safe reports until they realize they forgot to tell you about the Problem Dampener.
+   
+   The Problem Dampener is a reactor-mounted module that lets the reactor safety systems tolerate a single bad level in what would otherwise be a safe report. It's like the bad level never happened!
+   
+   Now, the same rules apply as before, except if removing a single level from an unsafe report would make it safe, the report instead counts as safe.
+   
+   More of the above example's reports are now safe:
+   
+   7 6 4 2 1: Safe without removing any level.
+   1 2 7 8 9: Unsafe regardless of which level is removed.
+   9 7 6 2 1: Unsafe regardless of which level is removed.
+   1 3 2 4 5: Safe by removing the second level, 3.
+   8 6 4 4 1: Safe by removing the third level, 4.
+   1 3 6 7 9: Safe without removing any level.
+   Thanks to the Problem Dampener, 4 reports are actually safe!
+   
+   Update your analysis by handling situations where the Problem Dampener can remove a single level from unsafe reports. How many reports are now safe?
+   
    */
-
+  
   func part2() -> Any {
-    return 0
+    return entities
+      .filter { safeCheck(for: $0, autoFix: true) }
+      .count
   }
 }
--- Tests/Day02.swift
+++ Tests/Day02.swift
@@ -11,7 +11,10 @@ struct Day02Tests {
     1 3 2 4 5
     8 6 4 4 1
     1 3 6 7 9
-
+    55 54 57 60 61
+    78 82 85 87 89 92 94 95
+    17 20 19 18 16 13 10 7
+    
     """
 
   @Test func testPart1() async throws {
@@ -22,6 +25,6 @@ struct Day02Tests {
 
   @Test func testPart2() async throws {
     let challenge = Day02(data: testData)
-    #expect(String(describing: challenge.part2()) == "99")
+    #expect(String(describing: challenge.part2()) == "7")
   }
 }
