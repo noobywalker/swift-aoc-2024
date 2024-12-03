78f3bfb88433300bd0a9b14cb995140c3699cd6e
Waratnan Suriyasorn <waratnan.suriyasorn@auto1.com>
2024-12-03 11:06:41 +0100

    Advent of code 2024 - Day03

--- Sources/Day02.swift
+++ Sources/Day02.swift
@@ -1,27 +1,27 @@
 import Algorithms
 
 struct Day02: AdventDay {
-  
+
   var data: String
-  
+
   var entities: [[Int]] {
     return data.split(separator: "\n")
       .map {
         $0.split(separator: " ").compactMap { Int($0) }
       }
   }
-  
+
   /**
    --- Day 2: Red-Nosed Reports ---
-   
+
    Fortunately, the first location The Historians want to search isn't a long walk from the Chief Historian's office.
-   
+
    While the Red-Nosed Reindeer nuclear fusion/fission plant appears to contain no sign of the Chief Historian, the engineers there run up to you as soon as they see you. Apparently, they still talk about the time Rudolph was saved through molecular synthesis from a single electron.
-   
+
    They're quick to add that - since you're already here - they'd really appreciate your help analyzing some unusual data from the Red-Nosed reactor. You turn to check if The Historians are waiting for you, but they seem to have already divided into groups that are currently searching every corner of the facility. You offer to help with the unusual data.
-   
+
    The unusual data (your puzzle input) consists of many reports, one report per line. Each report is a list of numbers called levels that are separated by spaces. For example:
-   
+
    7 6 4 2 1
    1 2 7 8 9
    9 7 6 2 1
@@ -29,13 +29,13 @@ struct Day02: AdventDay {
    8 6 4 4 1
    1 3 6 7 9
    This example data contains six reports each containing five levels.
-   
+
    The engineers are trying to figure out which reports are safe. The Red-Nosed reactor safety systems can only tolerate levels that are either gradually increasing or gradually decreasing. So, a report only counts as safe if both of the following are true:
-   
+
    The levels are either all increasing or all decreasing.
    Any two adjacent levels differ by at least one and at most three.
    In the example above, the reports can be found safe or unsafe by checking those rules:
-   
+
    7 6 4 2 1: Safe because the levels are all decreasing by 1 or 2.
    1 2 7 8 9: Unsafe because 2 7 is an increase of 5.
    9 7 6 2 1: Unsafe because 6 2 is a decrease of 4.
@@ -43,22 +43,22 @@ struct Day02: AdventDay {
    8 6 4 4 1: Unsafe because 4 4 is neither an increase or a decrease.
    1 3 6 7 9: Safe because the levels are all increasing by 1, 2, or 3.
    So, in this example, 2 reports are safe.
-   
+
    Analyze the unusual data from the engineers. How many reports are safe?
    */
-  
+
   enum TolerateLevelType {
     case increasing
     case decreasing
   }
-  
+
   private func isSafeFor(type: TolerateLevelType, current: Int, next: Int) -> Bool {
     let diff = next - current
     guard diff != 0, abs(diff) <= 3 else {
       // neither an increase or a decrease or greater than three
       return false
     }
-    
+
     switch type {
     case .increasing where diff < 0:
       return false
@@ -68,7 +68,7 @@ struct Day02: AdventDay {
       return true
     }
   }
-  
+
   private func safeCheck(for levels: [Int], autoFix: Bool = false) -> Bool {
     guard !levels.isEmpty else { return false }
     var type: TolerateLevelType = .increasing
@@ -78,21 +78,21 @@ struct Day02: AdventDay {
       if idx == 0 {
         type = next > current ? .increasing : .decreasing
       }
-      
+
       guard isSafeFor(type: type, current: current, next: next) else {
         guard autoFix else { return false }
         var dropFirst: [Int] = []
         var dropCurrent = levels
         var dropNext = levels
-        
+
         if idx > 0 {
           dropFirst = levels
           dropFirst.remove(at: 0)
         }
-        
+
         dropCurrent.remove(at: idx)
         dropNext.remove(at: idx+1)
-        
+
         return safeCheck(for: dropFirst)
         || safeCheck(for: dropCurrent)
         || safeCheck(for: dropNext)
@@ -100,24 +100,24 @@ struct Day02: AdventDay {
     }
     return true
   }
-  
+
   func part1() -> Any {
     return entities
       .filter { safeCheck(for: $0) }
       .count
   }
-  
+
   /**
    --- Part Two ---
-   
+
    The engineers are surprised by the low number of safe reports until they realize they forgot to tell you about the Problem Dampener.
-   
+
    The Problem Dampener is a reactor-mounted module that lets the reactor safety systems tolerate a single bad level in what would otherwise be a safe report. It's like the bad level never happened!
-   
+
    Now, the same rules apply as before, except if removing a single level from an unsafe report would make it safe, the report instead counts as safe.
-   
+
    More of the above example's reports are now safe:
-   
+
    7 6 4 2 1: Safe without removing any level.
    1 2 7 8 9: Unsafe regardless of which level is removed.
    9 7 6 2 1: Unsafe regardless of which level is removed.
@@ -125,11 +125,11 @@ struct Day02: AdventDay {
    8 6 4 4 1: Safe by removing the third level, 4.
    1 3 6 7 9: Safe without removing any level.
    Thanks to the Problem Dampener, 4 reports are actually safe!
-   
+
    Update your analysis by handling situations where the Problem Dampener can remove a single level from unsafe reports. How many reports are now safe?
-   
+
    */
-  
+
   func part2() -> Any {
     return entities
       .filter { safeCheck(for: $0, autoFix: true) }
